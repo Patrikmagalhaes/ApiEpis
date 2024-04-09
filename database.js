8dea1a169bd17dd911694b1dcae4274ff8cf8ca1
PATRIK DA SILVEIRA MAGALHÃES <patrikmagalhaes@icloud.com>
2024-04-08 21:31:56 -0300

    inicial

--- database.js
+++ database.js
@@ -8,3 +8,48 @@ database.connect((erro) => {
 });
 
 module.exports = database;
+
+
+/*
+
+Essa tabela armazenará informações sobre os funcionários.
+
+CREATE TABLE Funcionarios (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    cargo VARCHAR(100),
+);
+
+Tabela de EPIs
+
+CREATE TABLE Epis (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    quantidade_em_estoque INT
+);
+
+Tabela de Histórico de Retiradas e Devoluções
+
+CREATE TABLE Historico_Epis (
+    id INT PRIMARY KEY,
+    id_funcionario INT,
+    id_epi INT,
+    data_retirada DATE,
+    data_devolucao DATE,
+    FOREIGN KEY (id_funcionario) REFERENCES Funcionarios(id),
+    FOREIGN KEY (id_epi) REFERENCES Epis(id)
+);
+
+Tabela de Relacionamento Funcionário-EPI
+
+CREATE TABLE Funcionario_Epi (
+    id_funcionario INT,
+    id_epi INT,
+    PRIMARY KEY (id_funcionario, id_epi),
+    FOREIGN KEY (id_funcionario) REFERENCES Funcionarios(id),
+    FOREIGN KEY (id_epi) REFERENCES Epis(id)
+);
+
+
+*/
