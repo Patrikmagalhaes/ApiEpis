d1de911588932b8f43535eca0ec138575f8006ea
PATRIK DA SILVEIRA MAGALHÃES <patrik_magalhaes@estudante.sesisenai.org.br>
2024-04-11 21:58:19 -0300

    testando database

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const episRoutes = require('./routes/epis')
 const colaboradoresRoutes = require('./routes/colaboradores')
-
+const cors = require('cors');
 const port = 3000
 const app = express()
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
 
+
+app.use(bodyParser.json())
+app.use(cors());
 app.use('/epis', episRoutes)
 app.use('/colaboradores', colaboradoresRoutes)
 
